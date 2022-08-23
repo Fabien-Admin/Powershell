77c203c7c3490e23168ca88e9b8d87ae8214d1a1
IT Admin <60690796+Fabien-Admin@users.noreply.github.com>
2022-08-23 15:16:52 +0200

    Add files via upload

---
+++ Convert Files to Base64/Convert Base64 to Files.ps1
@@ -0,0 +1,11 @@
+﻿# Convert from Base64 to EXE
+$EXE_Path = "$env:TEMP\<EXE name.exe>"
+$Base64_Code = "TVqQAAMAAAAEAAAA//8AALgAAAAAAAAAQAAAAAA..."
+[byte[]]$Bytes = [convert]::FromBase64String($Base64_Code)
+[System.IO.File]::WriteAllBytes($EXE_Path,$Bytes)
+ 
+# Convert from Base64 to PS1
+$PS1_Path = "$env:TEMP\<PS1 name.ps1>"
+$Base64_Code = "TVqQAAMAAAAEAAAA//8AALgAAAAAAAAAQAAAAAA..."
+[byte[]]$Bytes = [convert]::FromBase64String($Base64_Code)
+[System.IO.File]::WriteAllBytes($PS1_Path,$Bytes)
---
+++ Convert Files to Base64/Convert File to Base64.ps1
@@ -0,0 +1,9 @@
+﻿# Convert from EXE to Base64
+$EXE_Path = <Path of your EXE>
+$Base64_Code = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$ServiceUI_Path"));
+$Base64_Code | Out-File "C:\Temp\ExeTo64.txt" 
+ 
+# Convert from PS1 to Base64
+$PS1_Path = "C:\Users\fdauvergne\Downloads\Olfeo Project\linux_OK.ps1"
+$Base64_Code = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$PS1_Path"));
+$Base64_Code | Out-File "C:\Temp\Ps1To64.txt"
