d27573cc40c80ce44ce6c7b6ff7022c3a5943ae2
IT Admin <60690796+Fabien-Admin@users.noreply.github.com>
2022-08-24 12:33:54 +0200

    Add files via upload

---
+++ Modification attributs de fichier.ps1
@@ -0,0 +1,11 @@
+﻿#Chemin du fichier à modifier
+$File = "C:\Temp\New.txt"
+
+#Modification de la date de création
+$(Get-Item $File).creationtime=$(Get-Date "01/01/2222 12:23")
+
+#Modification de la date du dernier accès
+$(Get-Item $File).lastaccesstime=$(Get-Date "22/01/2025 23:23")
+
+#Modification de la date de dernière modification
+$(Get-Item $File).lastwritetime=$(Get-Date "31/12/2107 23:59")
