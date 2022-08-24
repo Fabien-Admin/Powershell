10b64487e41a94c526aa4f30eee7e43171a0d950
IT Admin <60690796+Fabien-Admin@users.noreply.github.com>
2022-08-24 12:22:15 +0200

    Add files via upload

---
+++ Funny/MouseShaker.ps1
@@ -0,0 +1,33 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("system.windows.forms")
+
+$Cursor = [system.windows.forms.cursor]::Clip
+
+#[system.windows.forms.cursor]::Position = New-Object system.drawing.point(0,0)
+
+#[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Cursor.Width/2),($Cursor.Height/2))
+
+#Must add this line of code for [system.windows.forms.cursor]::Position to work outside of ISE
+#Thanks to Meik1988 on youtube for schooling me
+
+
+for ($i=1;$i -lt 5000;$i++) {
+
+$Position = [system.windows.forms.cursor]::Position
+
+$PositionChange = Get-Random 60
+
+switch (Get-Random 4) {
+
+    0 {[system.windows.forms.cursor]::Position = New-Object system.drawing.point($Position.x, ($Position.y + $PositionChange))}
+    1 {[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Position.x + $PositionChange),$Position.y)}
+    2 {[system.windows.forms.cursor]::Position = New-Object system.drawing.point($Position.x, ($Position.y - $PositionChange))}
+    3 {[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Position.x - $PositionChange),$Position.y)}
+
+}
+
+#$PositionChange
+$i
+
+}
+
+#>
