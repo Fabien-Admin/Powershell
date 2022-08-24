b6ba3795ee293ea16e0196e90267203205914e2a
IT Admin <60690796+Fabien-Admin@users.noreply.github.com>
2022-08-24 12:24:41 +0200

    Add files via upload

---
+++ Serveur Web/Web Server.ps1
@@ -0,0 +1,147 @@
+# This is a super **SIMPLE** example of how to create a very basic powershell webserver
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+function Get-WebServer {
+
+# Http Server
+$http = [System.Net.HttpListener]::new()
+
+# Hostname and port to listen on
+$http.Prefixes.Add("http://localhost:8080/")
+
+# Start the Http Server 
+$http.Start()
+
+
+
+# Log ready message to terminal 
+if ($http.IsListening) {
+    write-host " HTTP Server Ready! " -f 'black' -b 'gre'
+    write-host "now try going to $($http.Prefixes)" -f 'y'
+    write-host "then try going to $($http.Prefixes)some/form" -f 'y'
+    write-host "complete the form and click on Submit" -f 'y'
+}
+
+
+# INFINTE LOOP
+# Used to listen for requests
+try {
+    while ($http.IsListening) {
+
+
+
+        # Get Request Url
+        # When a request is made in a web browser the GetContext() method will return a request object
+        # Our route examples below will use the request object properties to decide how to respond
+        $contextTask = $http.GetContextAsync()
+
+        # Waits in 200ms increments for a request. We do this to allow pipeline stops to be processed (i.e. CTRL+C)
+        while (-not $contextTask.AsyncWaitHandle.WaitOne(200)) { }
+        $context = $contextTask.GetAwaiter().GetResult()
+
+
+        # ROUTE EXAMPLE 1
+        # http://127.0.0.1/
+        if ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq '/') {
+
+            # We can log the request to the terminal
+            write-host "$($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -f 'mag'
+
+            # the html/data you want to send to the browser
+            # you could replace this with: [string]$html = Get-Content "C:\some\path\index.html" -Raw
+            [string]$html = "<h1>A Powershell Webserver</h1><p>Home page</p>
+            <input type=button onclick=window.location.href='/some/form' title='Formulaire' value=Form />"
+        
+            #resposed to the request
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($html) # convert htmtl to bytes
+            $context.Response.ContentLength64 = $buffer.Length
+            $context.Response.OutputStream.Write($buffer, 0, $buffer.Length) #stream to broswer
+            $context.Response.OutputStream.Close() # close the response
+    
+        }
+
+
+
+        # ROUTE EXAMPLE 2
+        # http://127.0.0.1/some/form'
+        if ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq '/some/form') {
+
+            # We can log the request to the terminal
+            write-host "$($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -f 'mag'
+
+            [string]$html = "
+        <h1>A Powershell Webserver</h1>
+        <form action='/some/post' method='post'>
+            <p>A Basic Form</p>
+            <p>Fullname</p>
+            <input type='text' name='Fullname'>
+            <p>Message</p>
+            <textarea rows='4' cols='50' name='Message'></textarea>
+            <br>
+            <input type='submit' title='Envoyer' value='Submit'>
+        </form>
+        "
+
+            #resposed to the request
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($html) 
+            $context.Response.ContentLength64 = $buffer.Length
+            $context.Response.OutputStream.Write($buffer, 0, $buffer.Length) 
+            $context.Response.OutputStream.Close()
+        }
+
+        # ROUTE EXAMPLE 3
+        # http://127.0.0.1/some/post'
+        if ($context.Request.HttpMethod -eq 'POST' -and $context.Request.RawUrl -eq '/some/post') {
+
+            # decode the form post
+            # html form members need 'name' attributes as in the example!
+            $FormContent = [System.IO.StreamReader]::new($context.Request.InputStream).ReadToEnd()
+
+            # We can log the request to the terminal
+            write-host "$($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -f 'mag'
+
+            #Extract just the Fullname
+            $StartN = $FormContent.IndexOf("=")
+            $EndN = $FormContent.LastIndexOf("&")
+            $Full = $FormContent.Substring($StartN+1,$EndN-$StartN-1)
+                        
+            #Decode the message web
+            Add-Type -AssemblyName System.Web
+            $Fullname = [System.Web.HttpUtility]::UrlDecode($Full)
+            
+
+            #Extract just the message
+            $StartM = $FormContent.LastIndexOf("=")
+            $MessEnd = $FormContent.Substring($StartM+1)
+            
+            #Decode the message web 
+            Add-Type -AssemblyName System.Web
+            $Mess = [System.Web.HttpUtility]::UrlDecode($MessEnd)
+    
+            #Show the Fullname & Message
+            Write-host "Fullname : " -f gre -NoNewline
+            Write-host $Fullname -f gre
+            Write-host "Message : " -f gre -NoNewline
+            Write-host $Mess -f gre
+
+            # the html/data
+            [string]$html = "<h1>A Powershell Webserver</h1><p>Post Successful !</p>" 
+
+            #resposed to the request
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($html)
+            $context.Response.ContentLength64 = $buffer.Length
+            $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
+            $context.Response.OutputStream.Close() 
+        }
+
+
+        # powershell will continue looping and listen for new requests...
+
+    }
+}
+finally {
+    $http.Stop()
+}
+
+}
+
+Get-WebServer
